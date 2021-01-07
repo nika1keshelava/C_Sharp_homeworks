a0f3c30cbcf9019251771c4080df920cec7f366d
nika keshelava <nika.keshelava.1@btu.edu.ge>
2021-01-07 23:41:05 +0400

    Add files via upload

---
+++ homework11/homework11/ADO_1_1/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ADO_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 577);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "წაკითხვა";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1344, 339);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 399);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1343, 92);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 640);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "გაფილტვრა";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 697);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 51);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "გაუქმება";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 578);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(490, 41);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(260, 626);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(235, 52);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "დახარისხება";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(269, 697);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 51);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "გაუქმება";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(531, 637);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(231, 53);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "გამოთვლები";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 509);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 51);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "object";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1044, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 36);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1048, 547);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 36);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1044, 600);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 36);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1050, 649);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 36);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1050, 706);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 36);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(533, 695);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(228, 56);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "ძებნა";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(220, 509);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(570, 41);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 752);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ homework11/homework11/ADO_1_1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADO_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 = 
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            SqlDataAdapter adap_Shemk = new SqlDataAdapter("SELECT * FROM Shemkveti", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            adap_Shemk.Fill(ds_1, "Shemkveti");
+            connect_1.Close();
+            dataGridView1.DataSource = ds_1;
+            dataGridView1.DataMember = "Personali";
+            dataGridView2.DataSource = ds_1;
+            dataGridView2.DataMember = "Shemkveti";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            DataView dv_1 = new DataView(ds_1.Tables["Personali"]);
+            dv_1.RowFilter = "ganyofileba = 'სავაჭრო' AND asaki > 30";
+            //dv_1.RowFilter = "tarigi_dabadebis >= '01.01.1980'"; // >= dateTimePicker1.Text;
+            //dv_1.RowFilter = textBox1.Text;
+            dataGridView1.DataSource = dv_1;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            DataView dv_1 = new DataView(ds_1.Tables["Personali"]);
+            dv_1.RowFilter = "";  //  = textBox1.Text;
+            dataGridView1.DataSource = dv_1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            DataView dv_1 = new DataView(ds_1.Tables["Personali"]);
+            //dv_1.Sort = "gvari";
+            //dv_1.Sort = "gvari DESC"; // კლებადობით
+            dv_1.Sort = "gvari, qalaqi DESC";
+            //dv_1.RowFilter = textBox1.Text;
+            dataGridView1.DataSource = dv_1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            DataView dv_1 = new DataView(ds_1.Tables["Personali"]);
+            dv_1.Sort = "";  //  = textBox1.Text;
+            dataGridView1.DataSource = dv_1;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            //object count_pers = ds_1.Tables["Personali"].Compute("COUNT(gvari)", "");
+            object count_pers = ds_1.Tables["Personali"].Compute("COUNT(gvari)", "qalaqi = 'თბილისი'");
+            object sum_pers = ds_1.Tables["Personali"].Compute("SUM(xelfasi)", "");
+            object avg_pers = ds_1.Tables["Personali"].Compute("AVG(staji)", "asaki > 40");
+            object max_pers = ds_1.Tables["Personali"].Compute("MAX(asaki)", "");
+            object min_pers = ds_1.Tables["Personali"].Compute("MIN(asaki)", "");
+            label1.Text = count_pers.ToString();
+            label2.Text = sum_pers.ToString();
+            label3.Text = avg_pers.ToString();
+            label4.Text = max_pers.ToString();
+            label5.Text = min_pers.ToString();
+        }
+        
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int primary_key = int.Parse(textBox1.Text);
+            SqlConnection connect_1 =
+                new SqlConnection("server=ROMANI\\SQL_2012; database=Shekveta; Integrated Security = True");
+            connect_1.Open();
+            SqlDataAdapter adap_Pers = new SqlDataAdapter("SELECT * FROM Personali", connect_1);
+            DataSet ds_1 = new DataSet();
+            adap_Pers.Fill(ds_1, "Personali");
+            connect_1.Close();
+            DataColumn[] keys = new DataColumn[1];
+            keys[0] = ds_1.Tables["Personali"].Columns["personaliID"];
+            ds_1.Tables["Personali"].PrimaryKey = keys;
+            DataRow find_row = ds_1.Tables["Personali"].Rows.Find(primary_key);
+            if (find_row != null)
+                label1.Text = find_row["gvari"] + "  " + find_row["asaki"].ToString();
+            else label1.Text = "ვერ მოიძებნა";
+            connect_1.Close();
+        }
+    }
+}
